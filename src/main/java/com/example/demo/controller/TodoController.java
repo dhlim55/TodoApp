bba2e5f467797200ee9dac7fb33ada494ca8d17c
임다희 <dahee@dahee-lim-M1-Pro.local>
2023-03-27 16:16:45 +0900

    3/22 수업(2.2절 완료)

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -2,16 +2,15 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.TodoDTO;
+import com.example.demo.model.TodoEntity;
 import com.example.demo.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.swing.text.html.parser.Entity;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("todo")
@@ -33,5 +32,56 @@ public class TodoController {
 
 
         return ResponseEntity.ok().body(response);
+    }
+@PostMapping
+    public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto) {
+        try{
+            String temporaryUserId = "temporary-user";
+
+            //(1)TodoEntity로 변환
+            TodoEntity entity = TodoDTO.toEntity(dto);
+
+            //(2)ID를 null로 초기화한다.
+            entity.setId(null);
+
+            //(3)임시 사용자 아이디를 설정. 인증과 인가 기능이 없으므로 한 사용자만 로그인 없이 사용할 수 있는 어플리케이션인것이다.
+            entity.setUserId(temporaryUserId);
+
+            //(4)
+            List<TodoEntity> entities = service.create(entity);
+
+            //(5)
+            List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+            //(6)
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+            //(7)
+            return ResponseEntity.ok().body(response);
+
+        } catch (Exception e){
+            //(8)혹시 예외가 있는 경우 dto대신 error에 메시지를 넣어 리턴
+            String error = e.getMessage();
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+
+
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
+    @GetMapping
+    public ResponseEntity<?> retrieveTodoList() {
+        String temporaryUserId = "temporary-user";
+
+        List<TodoEntity> entities = service.retrieve(temporaryUserId);
+
+        List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+        //(6)
+        ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+        //(7)
+        return ResponseEntity.ok().body(response);
+
+
     }
 }
--- src/main/java/com/example/demo/persistence/TodoRepository.java
+++ src/main/java/com/example/demo/persistence/TodoRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface TodoRepository extends JpaRepository<TodoEntity, String> {
     List<TodoEntity> findByUserId(String userId);
 
+
 }
